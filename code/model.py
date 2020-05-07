3f675a460d25e67432f027c0d2d40c1b352d9c16
simonsirak <simon.sirak@gmail.com>
2020-05-07 17:22:12 +0200

    Finalize sketch of PPM

--- code/model.py
+++ code/model.py
@@ -6,11 +6,11 @@ import tensorflow_datasets as tfds
 
 from load_data import DataLoader
 
-loader = DataLoader(split="100%")
+loader = DataLoader(dataset_name='cityscapes', split="100")
 loader.normalizeAllData()
 
 train_ds = loader.getTrainData()
-train_ds = train_ds.shuffle(128)
+# train_ds = train_ds.shuffle(128)
 
 val_ds = loader.getValData()
 
@@ -22,19 +22,31 @@ class MyModel(tf.keras.Model):
         super(MyModel, self).__init__(name="baseWithPyramidPooling")
 
         # base model
-        self.base_model = tf.keras.applications.resnet.ResNet50(input_shape=(1024, 2048, 3), weights="imagenet", include_top=False)
+        self.base_model = tf.keras.applications.resnet.ResNet50(weights="imagenet", include_top=False)
         self.base_model.trainable = True
 
         # upsample (just temporary)
         self.up0 = tf.keras.layers.UpSampling2D(size=(4, 4), interpolation="bilinear")
 
         # pyramid pooling module
-        self.ppool1 = tf.keras.layers.MaxPooling2D((128, 256), strides=(1, 1))
-        self.ppool2 = tf.keras.layers.MaxPooling2D((127, 255), strides=(1, 1))
-        self.ppool3 = tf.keras.layers.MaxPooling2D((126, 254), strides=(1, 1))
-        self.ppool6 = tf.keras.layers.MaxPooling2D((123, 251), strides=(1, 1))
+        # 
+        # Stride = (input_size//output_size)
+        # Kernel size = input_size - (output_size-1)*stride
+        # Padding = 0
 
-        self.pconv = tf.keras.layers.Conv2D(1, (1, 1), activation="relu")
+        self.features = []
+
+        bins = [1,2,3,6]
+        inp_dim = (128,256)
+        for bin_ in bins:
+          strides = (inp_dim[0] // bin_, inp_dim[1] // bin_)
+          kernel_size = (inp_dim[0] - (bin_-1)*strides[0], inp_dim[1] - (bin_-1)*strides[1])
+          self.features.append(tf.keras.Sequential([
+            tf.keras.layers.MaxPooling2D(kernel_size, strides=strides),
+            tf.keras.layers.Conv2D(512, (1, 1)),
+            tf.keras.layers.BatchNormalization(),
+            tf.keras.layers.ReLU()
+          ]))
 
         # concatenation layer
         self.concat0 = tf.keras.layers.Concatenate()
@@ -53,30 +65,24 @@ class MyModel(tf.keras.Model):
         feature_map = self.base_model(input)
         feature_map_upsampled = self.up0(feature_map)
         
-        bin_pool1 = self.ppool1(feature_map_upsampled)
-        bin_pool2 = self.ppool2(feature_map_upsampled)
-        bin_pool3 = self.ppool3(feature_map_upsampled)
-        bin_pool6 = self.ppool6(feature_map_upsampled)
+        poolings = [feature_map_upsampled]
+        for f in self.features:
+          poolings.append(tf.image.resize(f(feature_map_upsampled), [128, 256]))
         
-        bin_pool1_conv = self.pconv(bin_pool1)
-        bin_pool2_conv = self.pconv(bin_pool2)
-        bin_pool3_conv = self.pconv(bin_pool3)
-        bin_pool6_conv = self.pconv(bin_pool6)
-        
-        bin_pool1_conv_upsampled = tf.image.resize(bin_pool1_conv, [128, 256])
-        bin_pool2_conv_upsampled = tf.image.resize(bin_pool2_conv, [128, 256])
-        bin_pool3_conv_upsampled = tf.image.resize(bin_pool3_conv, [128, 256])
-        bin_pool6_conv_upsampled = tf.image.resize(bin_pool6_conv, [128, 256])
-        
-        feature_map_bins_concat = self.concat0([feature_map_upsampled, bin_pool1_conv_upsampled, bin_pool2_conv_upsampled, bin_pool3_conv_upsampled, bin_pool6_conv_upsampled])
+        feature_map_bins_concat = self.concat0(poolings)
+        print("F MAP " + str(feature_map_bins_concat))
         output_num_classes_depth = self.conv0(feature_map_bins_concat)
         output_upsampled = self.up1(output_num_classes_depth)
         output_softmax = self.soft0(output_upsampled)
+        print("OUTPUT " + str(output_softmax))
         
         return output_softmax
       
 model = MyModel(30)
-train_ds = train_ds.batch(2)
+train_ds = train_ds.batch(1)
+val_ds = val_ds.batch(1)
 
 model.compile(optimizer="SGD", loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False), metrics=["accuracy"])
-model.fit(x=train_ds, validation_data=val_ds, epochs=3)
+#model.build(input_shape=(None,1024,2048,3))
+model.fit(x=train_ds, validation_data=val_ds, epochs=3)                                 
+model.summary(line_length=90)
